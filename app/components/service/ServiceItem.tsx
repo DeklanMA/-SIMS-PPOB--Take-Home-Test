5509391b568c3187f964590e8d8134414feeaf20
DeklanMA <deklanmalik@gmail.com>
2025-12-19 09:38:59 +0700

    feat:hadle lihat saldo defaut tutup

--- app/components/service/ServiceItem.tsx
+++ app/components/service/ServiceItem.tsx
@@ -12,10 +12,10 @@ export default function ServiceItem({code, name, icon}: Props) {
   return (
     <button
       onClick={() => navigate(`/service/${code}`)}
-      className="flex cursor-pointer flex-col items-center gap-2 min-w-20  rounded-lg hover:bg-gray-100 transition"
+      className="flex cursor-pointer flex-col items-center gap-2 min-w-16  rounded-lg hover:bg-gray-100 transition"
     >
       <img src={icon} alt={name} className="w-16 h-16 object-cover border border-gray-200 rounded-lg" />
-      <span className="text-sm text-center">{name}</span>
+      <span className="text-xs text-center">{name}</span>
     </button>
   );
 }
